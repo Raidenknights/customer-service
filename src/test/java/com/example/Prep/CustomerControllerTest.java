ea813e73752c50481c1dece2e266bfec3d98c3fd
Raidenknights <manas.vishnoi@gmail.com>
2024-05-24 19:17:09 +0530

    Adding service layer test case

---
+++ src/test/java/com/example/Prep/CustomerControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.Prep;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.Prep.controller.CustomerController;
+import com.example.Prep.service.CustomerService;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerControllerTest {
+
+	@Mock
+	CustomerService customerService;
+
+	@InjectMocks
+	CustomerController customerController;
+
+	@Test
+	public void fetchCustomerTest() {
+
+	}
+
+}
