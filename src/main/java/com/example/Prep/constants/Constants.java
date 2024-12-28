5cac075dfb6e4839c0322a6899c4ac24a2b5cb29
Raidenknights <manas.vishnoi@gmail.com>
2024-12-28 20:01:05 +0530

    Adding logger and dependency for logger in pom.xml

--- src/main/java/com/example/Prep/constants/Constants.java
+++ src/main/java/com/example/Prep/constants/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 	
 	public static final String CREATE_CUSTOMER_URL="customer/new/create";
 	public static final String FETCH_CUSTOMER_DETAIL_URL="customer/fetch";
+	public static final String CHECK_IF_CUSTOMER_EXISTS="/fetch/user/exists/{email}";
 	
 
 }
--- src/main/java/com/example/Prep/controller/CustomerController.java
+++ src/main/java/com/example/Prep/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.example.Prep.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -14,13 +16,17 @@ import com.example.Prep.dto.request.Customer;
 import com.example.Prep.service.CustomerService;
 
 /*
- * This class is only for CRUD API for the customer
+ * This class the controller class and exposes API to perform Create and fetch operations for customer.
+ * @author: Manas Vishnoi
  */
 @RestController
 public class CustomerController {
 
+	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
 	private final CustomerService customerService;
 
+	// constructor based dependency
 	public CustomerController(CustomerService customerService) {
 		this.customerService = customerService;
 	}
@@ -28,19 +34,22 @@ public class CustomerController {
 	@GetMapping(path = Constants.FETCH_CUSTOMER_DETAIL_URL)
 	public ResponseEntity<Customer> fetchCustomerByUserName(Authentication auth) {
 
+		logger.debug("Entered fetchCustomerByUserName method");
 		return new ResponseEntity<>(customerService.readCustomer(auth.getName()), HttpStatus.OK);
 
 	}
 
-	@GetMapping(path = "/fetch/user/exists/{email}")
+	@GetMapping(path = Constants.CHECK_IF_CUSTOMER_EXISTS)
 	public ResponseEntity<Boolean> checkIfCustomerExists(@PathVariable String email) {
 
+		logger.debug("Entered checkIfCustomerExists method");
 		return new ResponseEntity<>(customerService.checkIfCustomerExists(email), HttpStatus.OK);
 	}
 
 	@PostMapping(path = Constants.CREATE_CUSTOMER_URL)
 	public ResponseEntity<String> createUser(@RequestBody Customer customer) {
 
+		logger.debug("Entered createUser method");
 		customerService.addCustomer(customer);
 		return new ResponseEntity<>("User has been created", HttpStatus.CREATED);
 	}
