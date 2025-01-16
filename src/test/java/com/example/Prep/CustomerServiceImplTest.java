5d97866be439325fd9dd26a54ff7025c5db4b423
Raidenknights <manas.vishnoi@gmail.com>
2025-01-16 23:28:35 +0530

    Adding customer service test

--- src/test/java/com/example/Prep/CustomerServiceImplTest.java
+++ src/test/java/com/example/Prep/CustomerServiceImplTest.java
@@ -1,9 +1,14 @@
 package com.example.Prep;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.Prep.dto.request.Customer;
 import com.example.Prep.entity.CustomerEntity;
@@ -11,15 +16,79 @@ import com.example.Prep.repository.CustomerRepository;
 import com.example.Prep.service.impl.CustomerServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
-public class CustomerServiceImplTest {
+class CustomerServiceImplTest {
 
 	@Mock
 	CustomerRepository customerRepository;
+
+	@Mock
+	PasswordEncoder passwordEncoder;
 
 	@InjectMocks
 	CustomerServiceImpl customerServiceImpl;
 
 	private Customer customer;
 	private CustomerEntity customerEntity;
+
+	@Test
+	void addCustomerTest() {
+
+		customer = new Customer();
+
+		customer.setCustomerId("something");
+		customer.setEmail("soomething@email.com");
+		customer.setEnabled(false);
+		customer.setPassword("somepassword");
+		customer.setUsername("some-username");
+
+		customerEntity = new CustomerEntity();
+
+		Mockito.when(customerRepository.save(Mockito.any(CustomerEntity.class))).thenReturn(customerEntity);
+
+		customerServiceImpl.addCustomer(customer);
+
+		Mockito.validateMockitoUsage();
+
+	}
+
+	@Test
+	void readCustomerTest() {
+
+		customerEntity = new CustomerEntity();
+
+		customerEntity.setCustomerId("someid");
+		customerEntity.setEmail("someemail");
+		customerEntity.setEnabled(false);
+
+		Mockito.when(customerRepository.findByusername("someusername")).thenReturn(customerEntity);
+
+		customerServiceImpl.readCustomer("someusername");
+
+		assertEquals(customerEntity.getCustomerId(), "someid");
+
+	}
+
+	@Test
+	void checkIfUserExistsTest() {
+
+		customerEntity = new CustomerEntity();
+
+		customerEntity.setCustomerId("someid");
+		customerEntity.setEmail("someemail");
+		customerEntity.setEnabled(false);
+		customerEntity.setRole("somerole");
+
+		Mockito.when(customerRepository.findByemail("someemail")).thenReturn(customerEntity);
+
+		boolean response = customerServiceImpl.checkIfCustomerExists("someemail");
+
+		assertEquals(true, response);
+
+		Mockito.when(customerRepository.findByemail("someemail")).thenReturn(null);
+		boolean responseFalse = customerServiceImpl.checkIfCustomerExists("someemail");
+
+		assertEquals(false, responseFalse);
+
+	}
 
 }
